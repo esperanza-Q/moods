30c69c35ead47052394ff4b99f79c94aeaf7cf5f
riveryunny <gayunphone@gmail.com>
2025-03-05 14:36:40 +0900

    select

--- js/select_detail.js
+++ js/select_detail.js
@@ -1,14 +1,11 @@
-// 모달 열기
 function openModal() {
     document.getElementById("reviewModal").style.display = "flex";
 }
 
-// 모달 닫기
 function closeModal() {
     document.getElementById("reviewModal").style.display = "none";
 }
 
-// 배경 클릭 시 모달 닫기
 window.onclick = function(event) {
     let modal = document.getElementById("reviewModal");
     if (event.target === modal) {
